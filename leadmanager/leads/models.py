e0ff9ad72ef1ac2028d1f8fb59da55ec1dd8c57d
MingiJ <jeffawesome.ja@gmail.com>
2022-03-29 11:49:37 +0300

    set up django rest api and react frontend

---
+++ leadmanager/leads/models.py
@@ -0,0 +1,11 @@
+from operator import mod
+from django.db import models
+
+class Lead(models.Model):
+    names = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# Create your models here.
